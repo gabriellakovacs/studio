23af616e9484c361663c47a8852c97acb6aebb79
gabriella kovacs <kovacsxgabriella@gmail.com>
2015-10-27 00:57:57 +0000

    modified js so that the grid padding-right = 0.5* .box width

--- optimize-grid-layout.js
+++ optimize-grid-layout.js
@@ -5,9 +5,10 @@ var rightMargin = 20;
 
 
 window.onresize = function (){
-    var availableW = window.innerWidth - side - rightMargin;
+    var availableW = window.innerWidth - side - rightMargin-50;
     var availableH = window.innerHeight * 0.85;
     document.getElementById("grid").style.width = (availableW).toString().concat("px");
+    
     
     var nrOfBoxes = document.querySelectorAll('#grid .box').length;
     var gridDimensions = [];
@@ -54,13 +55,15 @@ window.onresize = function (){
         }
     }
     
-    
+    document.getElementById("grid").style.paddingRight = '50px';
 };
 
 window.onload = function (){
+    
     var availableW = window.innerWidth - side - rightMargin;
     var availableH = window.innerHeight * 0.85;
     document.getElementById("grid").style.width = (availableW).toString().concat("px");
+    
     
     var nrOfBoxes = document.querySelectorAll('#grid .box').length;
     var gridDimensions = [];
@@ -78,7 +81,7 @@ window.onload = function (){
     
      for (i = 0; i < gridDimensions.length; i++) {
         if (bestFit.length === 0 || 
-            Math.min(availableW / gridDimensions[i][0], availableH / gridDimensions[i][1]) > Math.min(availableW / bestFit[0], availableH / bestFit[1])) {
+            Math.min(availableW / (gridDimensions[i][0] + 0.5), availableH / gridDimensions[i][1]) > Math.min(availableW / (bestFit[0] + 0.5), availableH / bestFit[1])) {
             bestFit = [gridDimensions[i][0], gridDimensions[i][1]];
         } 
      }
@@ -86,7 +89,7 @@ window.onload = function (){
      
     
     if (availableW / bestFit[0] <= availableH / bestFit[1]) {
-        var viewportRatio = availableW / window.innerWidth / bestFit[0] * 75;
+        var viewportRatio = availableW / window.innerWidth / (bestFit[0] + 0.5) * 75;
         var dimension = "vw";
     } else {
         var viewportRatio = availableH / window.innerHeight / bestFit[1] * 75;
@@ -107,5 +110,5 @@ window.onload = function (){
         }
     }
     
-    
+    document.getElementById("grid").style.paddingRight = (viewportRatio / 75 * 100).toString().concat(dimension);
 };
