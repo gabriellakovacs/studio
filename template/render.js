af48c99c8d9d11b4b576208939b20a57dd6f0576
gabriella kovacs <kovacsxgabriella@gmail.com>
2015-10-28 19:26:11 +0000

    template ready for testing

--- template/render.js
+++ template/render.js
@@ -1,5 +1,18 @@
-var swig  = require('swig');
-swig.renderFile('/template/template.html', {
-    pagename: 'awesome people',
-    authors: ['Paul', 'Jim', 'Jane']
+var swig  = require("swig");
+swig.renderFile("/template/template.html", {
+
+	mainProjectTitle: "Drawings that Count",
+	mainProjectFolder: "drawings-that-count",
+	mainProjectPublisher:"Architectural Association Publications, 2013",
+    coverImageSrc: "../images/projects/"+ mainProjectFolder + "/cover.jpg",
+    coverImageAlt: "Cover of the book: 'Drawings that count'",
+    subProjectTitle: "",
+    subProjectAuthor:"",
+    subProjectInfo:"",
+    subProjectImg:"",
+    aboutProject:"",
+    nextProjectHref:"" + ".html",
+    prevProjectHref:"" + ".html",
+    mainImgSrc: "../images/projects/" + mainProjectFolder + "/" + subProjectImg + ".jpg",
+    mainImgZoomSrc: "../images/projects/" + mainProjectFolder + "/zoom/" + subProjectImg + ".jpg"
 });
