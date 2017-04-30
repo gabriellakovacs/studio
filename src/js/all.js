72d27fbc04059e739d6cc5c12fd0ff2b2567ca64
gabriellakovacs <kovacsxgabriella@gmail.com>
2017-04-30 22:50:15 +0200

    make aeolian same height as image next to it

--- src/js/all.js
+++ src/js/all.js
@@ -39,7 +39,7 @@ var lowerImages = [
 
     upperImages = [
         ["sufferNoFools", 250], 
-        ["aeolian", 375], 
+        ["aeolian", 369], 
         ["howTheDrawingThatCantForgetForgot", 250],
         ["estate", 150], 
         ["cowboy", 150],
